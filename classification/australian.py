7fef90b2d6f49f84bd2dc3ceb5e2efdb38d349a8
hlin117 <hlin117@gmail.com>
2015-07-18 22:58:41 -0700

    removed main function from australian

--- classification/australian.py
+++ classification/australian.py
@@ -19,34 +19,31 @@ SEED = 888
 np.random.seed(SEED)
 numFolds = 10
 
-def main():
+dataset = pd.read_csv("data/australian.data", header=None, sep=" ")
+X = dataset.drop(14, axis=1)
+Y = dataset[14]
 
-    dataset = pd.read_csv("data/australian.data", header=None, sep=" ")
-    X = dataset.drop(14, axis=1)
-    Y = dataset[14]
-    conv_X = pd.get_dummies(X, columns=[0, 3, 4, 5, 7, 8, 10, 11])
-    kf = KFold(len(X), numFolds, shuffle=True)
+# Produces a "one hot encoding" of the data
+conv_X = pd.get_dummies(X, columns=[0, 3, 4, 5, 7, 8, 10, 11])
+kf = KFold(len(X), numFolds, shuffle=True)
 
-    # These are "Class objects". For each Class, find the AUC through
-    # 10 fold cross validation.
-    Models = [LogisticRegression, RandomForestClassifier, SVC]
-    params = [{}, {}, {"probability": True}]
-    for Model, param in zip(Models, params):
-        total = 0
-        for train_indices, test_indices in kf:
+# These are "Class objects". For each Class, find the AUC through
+# 10 fold cross validation.
+Models = [LogisticRegression, RandomForestClassifier, SVC]
+params = [{}, {}, {"probability": True}]
+for Model, param in zip(Models, params):
+    total = 0
+    for train_indices, test_indices in kf:
 
-            # Get the dataset; this is the way to access values in a pandas DataFrame
-            train_X = conv_X.ix[train_indices, :]; train_Y = Y[train_indices]
-            test_X = conv_X.ix[test_indices, :]; test_Y = Y[test_indices]
+        # Get the dataset; this is the way to access values in a pandas DataFrame
+        train_X = conv_X.ix[train_indices, :]; train_Y = Y[train_indices]
+        test_X = conv_X.ix[test_indices, :]; test_Y = Y[test_indices]
 
-            # Train the model, and evaluate it
-            reg = Model(**param)
-            reg.fit(train_X, train_Y)
-            predictions = reg.predict_proba(test_X)[:, 1]
-            fpr, tpr, _ = roc_curve(test_Y, predictions)
-            total += auc(fpr, tpr)
-        accuracy = total / numFolds
-        print "AUC of {0}: {1}".format(Model.__name__, accuracy)
-
-if __name__ == "__main__":
-    main()
+        # Train the model, and evaluate it
+        reg = Model(**param)
+        reg.fit(train_X, train_Y)
+        predictions = reg.predict_proba(test_X)[:, 1]
+        fpr, tpr, _ = roc_curve(test_Y, predictions)
+        total += auc(fpr, tpr)
+    accuracy = total / numFolds
+    print "AUC of {0}: {1}".format(Model.__name__, accuracy)
